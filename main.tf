97d9e02913bdbe534826b0d9c2963382e69a5a07
gasasains26 <gasasains26@outlook.com>
2025-05-02 13:43:10 +0530

    Add Terraform code and GitHub Actions

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "key" {
+  key_name   = "terraform-key-1"
+  public_key = file("${path.module}/id_rsa.pub")
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-0c02fb55956c7d316"  # Ubuntu AMI in us-east-1
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.key.key_name
+
+  tags = {
+    Name = "UbuntuFromGitHub"
+  }
+}
+
